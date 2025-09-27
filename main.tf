f54e098b22b39f537f4f48fafb205766e18e18a1
LucasRamos7 <86800737+LucasRamos7@users.noreply.github.com>
2025-09-27 00:52:09 -0300

    refactor: increase node amount

--- main.tf
+++ main.tf
@@ -92,9 +92,9 @@ resource "aws_eks_node_group" "eks-node" {
   instance_types  = [var.instance_type]
 
   scaling_config {
-    desired_size = 3
+    desired_size = 4
     min_size     = 1
-    max_size     = 3
+    max_size     = 4
   }
 
   update_config {
